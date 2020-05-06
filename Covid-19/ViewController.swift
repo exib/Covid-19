50d8b5daceaea3f6cf7bf2cb13e6953551ca8756
Goncharov Andrey <goncharov.live@gmail.com>
2020-05-06 22:55:46 +0300

    Initial Commit

---
+++ Covid-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19
+//
+//  Created by Andrey Goncharov on 06.05.2020.
+//  Copyright © 2020 Andrey Goncharov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
